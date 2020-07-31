41c5a6d57fa95902d391f25af80ec44eadc1a572
Arnaldo Alfonso Pacheco Sarmiento <p.arnaldoalfonso@hotmail.com>
2020-07-31 11:36:35 -0500

    Se agregaron los datos de quienes somos,los servicios y se quitaro los link inecesarios.

---
+++ MLS.Web/Controllers/ConceptosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MLS.Web.Controllers
+{
+    public class ConceptosController : Controller
+    {
+        // GET: ConceptosController
+        public ActionResult Diagnostico()
+        {
+            return View();
+        }
+
+        public ActionResult Tecnico()
+        {
+            return View();
+        }
+
+
+    }
+}
